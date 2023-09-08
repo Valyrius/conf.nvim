2ffb05361699f61954959d061e918c8773fe2537
Valyrius <51477242+Valyrius@users.noreply.github.com>
2023-09-08 21:27:53 +0300

    improve indentation for pm.lua

--- lua/modules/pm.lua
+++ lua/modules/pm.lua
@@ -13,42 +13,42 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
     {'Mofiqul/dracula.nvim',
-    config = function()
-        vim.cmd('colorscheme dracula')
-    end},
+        config = function()
+            vim.cmd('colorscheme dracula')
+        end},
 
     'theprimeagen/harpoon',
     'mbbill/undotree',
     'andweeb/presence.nvim',
 
     {'nvim-lualine/lualine.nvim',
-    dependencies = {'nvim-tree/nvim-web-devicons'}},
+        dependencies = {'nvim-tree/nvim-web-devicons'}},
 
     {'nvim-telescope/telescope.nvim',
-	branch = '0.1.x',
-	dependencies = {'nvim-lua/plenary.nvim'}},
+        branch = '0.1.x',
+        dependencies = {'nvim-lua/plenary.nvim'}},
 
     {'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate'},
+        build = ':TSUpdate'},
+
     'nvim-treesitter/nvim-treesitter-context',
 
     {'VonHeikemen/lsp-zero.nvim',
-    branch = 'v1.x',
-    dependencies = {
-        -- LSP Support
-        'neovim/nvim-lspconfig',
-        'williamboman/mason.nvim',
-        'williamboman/mason-lspconfig.nvim',
-        -- Autocompletion
-        'hrsh7th/nvim-cmp',
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-path',
-        'saadparwaiz1/cmp_luasnip',
-        'hrsh7th/cmp-nvim-lsp',
-        'hrsh7th/cmp-nvim-lua',
-        -- Snippets
-        'L3MON4D3/LuaSnip',
-        'rafamadriz/friendly-snippets',
-    }}
+        branch = 'v1.x',
+        dependencies = {
+            -- LSP Support
+            'neovim/nvim-lspconfig',
+            'williamboman/mason.nvim',
+            'williamboman/mason-lspconfig.nvim',
+            -- Autocompletion
+            'hrsh7th/nvim-cmp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'saadparwaiz1/cmp_luasnip',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-nvim-lua',
+            -- Snippets
+            'L3MON4D3/LuaSnip',
+            'rafamadriz/friendly-snippets'}}
 })
 
