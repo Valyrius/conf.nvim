a2fdbbd87cf9c48b8134edee733a53fc444f94b3
Valyrius <51477242+Valyrius@users.noreply.github.com>
2023-09-08 16:13:04 +0300

    improved harpoon config

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,7 +1,20 @@
+local main = require("harpoon")
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
+main.setup({
+    tabline = true
+})
+
 vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-vim.keymap.set("n", "<C-s>", function() ui.nav_next() end)
+vim.keymap.set("n", "<A-w>", ui.toggle_quick_menu)
+
+vim.keymap.set("n", "<A-d>", ui.nav_next)
+vim.keymap.set("n", "<A-a>", ui.nav_prev)
+
+vim.cmd('highlight! HarpoonInactive guibg=NONE guifg=#63698c')
+vim.cmd('highlight! HarpoonActive guibg=NONE guifg=white')
+vim.cmd('highlight! HarpoonNumberActive guibg=NONE guifg=#7aa2f7')
+vim.cmd('highlight! HarpoonNumberInactive guibg=NONE guifg=#7aa2f7')
+vim.cmd('highlight! TabLineFill guibg=NONE guifg=white')
 
