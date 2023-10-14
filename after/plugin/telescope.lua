b194104c7b9decdc3a63ce71e7d8796d5e3f2acb
Valyrius <51477242+Valyrius@users.noreply.github.com>
2023-10-14 16:58:47 +0300

    changed telescope binds and added live_grep

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,8 +1,7 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
-vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>w', builtin.find_files, {})
+vim.keymap.set('n', '<leader>q', builtin.git_files, {})
+vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
 
+-- live_grep requires ripgrep binary (sudo pacman -S ripgrep)
+vim.keymap.set('n', '<leader>e', builtin.live_grep, {})
