fe9262431603faf714ea79a18bd1961c69ac5474
Valyrius <51477242+Valyrius@users.noreply.github.com>
2024-01-08 18:39:20 +0200

    moving to nvimcord

---
+++ after/plugin/nvimcord.lua
@@ -0,0 +1,5 @@
+require('nvimcord').setup {
+    autostart = true,
+    client_id = '954365489214291979',
+    large_file_icon = true,
+}
--- lua/modules/pm.lua
+++ lua/modules/pm.lua
@@ -14,7 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     'theprimeagen/harpoon',
     'mbbill/undotree',
-    'andweeb/presence.nvim',
+    'ObserverOfTime/nvimcord',
     'RRethy/vim-illuminate',
 
     {'bluz71/vim-moonfly-colors',
